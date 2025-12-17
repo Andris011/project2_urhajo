bf55cadb00ad5be659be1810c44d32a689229d75
Andris011 <andris.febert111@gmail.com>
2025-12-17 15:45:26 +0100

    kész

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ class Program
     const string P = "|O|"; // player
     const string E = "|V|"; // enemy
     const string F = "|";
+    const string B = "|^|";
 
-    static List<string> ures_sor = new List<string> {F,S,S,S,S,S,S,S,S,S,F};
-    
+    static List<string> ures_sor = new List<string> { F, S, S, S, S, S, S, S, S, S, F };
+
     static string[,] map =
     {
         {F,S,S,S,S,S,S,S,S,S,F},
@@ -21,17 +22,27 @@ class Program
         {F,S,S,S,S,S,S,S,S,S,F},
         {F,S,S,S,S,P,S,S,S,S,F}
     };
-    
+
     static Random r = new Random();
+
+    static int enemySzam = 0;
 
     static void AddEnemy()
     {
-        HashSet<int> hol = new HashSet<int>();
-        
-        for (int i = 0; i < 6; i++)
+
+        enemySzam = 6;
+
+        List<int> hol = new List<int>();
+
+        while (hol.Count < enemySzam)
         {
-            
-            hol.Add(r.Next(1, 10));
+            int pos = (r.Next(1, 10));
+
+            if (!hol.Contains(pos))
+            {
+                hol.Add(pos);
+            }
+
         }
 
         foreach (var elem in hol)
@@ -42,123 +53,239 @@ class Program
 
     static void Kiiras()
     {
-        Console.Clear();
+
+        string ki = "\x1B[H \n";
+
         for (int i = 0; i < map.GetLength(0); i++)
         {
             for (int j = 0; j < map.GetLength(1); j++)
             {
-                Console.Write($"{map[i, j]}");
+                ki += ($"{map[i, j]}");
             }
 
-            Console.WriteLine();
+            ki += "\n";
         }
-    }
 
+        Console.WriteLine(ki);
+    }
 
 
     static void Mozgas()
     {
         Console.WriteLine("Üdvözöllek a játékban! Nyomj egy gombot a folytatáshoz!");
-        
+
         ConsoleKeyInfo pressed_key = Console.ReadKey(true);
-        
-        
+
+        Console.Clear();
+
+        AddEnemy();
+
         bool kilepes = false;
-        
+
+        int mozgasCount = 0;
+        int lovesCount = 0;
+
+
+        int player_x = 0, player_y = 0;
+
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+
+                if (map[i, j] == P)
+                {
+                    player_y = i;
+                    player_x = j;
+                }
+                // Console.Write($"{map[i, j]}");
+            }
+            // Console.WriteLine();
+        }
+
         do
         {
-            Kiiras();
-            
-            int player_x = 0, player_y = 0;
-        
-            pressed_key = Console.ReadKey(true);
 
-            Console.Clear();
-            
-            for (int i = 0; i < map.GetLength(0); i++)
+            if (Console.KeyAvailable)
             {
-                for (int j = 0; j < map.GetLength(1); j++)
+
+                pressed_key = Console.ReadKey(true);
+
+
+                if (pressed_key.Key == ConsoleKey.Escape) kilepes = true;
+
+                if (pressed_key.Key == ConsoleKey.RightArrow)
                 {
-                    
-                    if (map[i, j] == P)
+                    map[player_y, player_x] = S;
+
+                    if (map[player_y, player_x + 1] != F)
                     {
-                        player_y = i;
-                        player_x = j;
+                        player_x++;
+                        map[player_y, player_x] = P;
                     }
-                    // Console.Write($"{map[i, j]}");
+                    else
+                    {
+                        player_x = 1;
+                        map[player_y, player_x] = P;
+                    }
                 }
-                // Console.WriteLine();
-            }
 
-            if (pressed_key.Key == ConsoleKey.RightArrow)
-            {
-                if (map[player_y, player_x + 1] != F)
+                if (pressed_key.Key == ConsoleKey.LeftArrow)
                 {
                     map[player_y, player_x] = S;
-                    map[player_y, player_x++] = P;
+
+                    if (map[player_y, player_x - 1] != F)
+                    {
+                        player_x--;
+                        map[player_y, player_x] = P;
+                    }
+                    else
+                    {
+                        player_x = 9;
+                        map[player_y, player_x] = P;
+                    }
+                }
+
+                if (pressed_key.Key == ConsoleKey.Spacebar)
+                {
+                    if (map[player_y - 1, player_x] == E)
+                    {
+                        enemySzam--;
+                        map[player_y - 1, player_x] = S;
+                    }
+                    else
+                    {
+                        map[player_y - 1, player_x] = B;
+                    }
+
+
+                    
+
+                }
+            }
+
+            lovesCount++;
+
+            if (lovesCount >= 12)
+            {
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        if (map[i, j] == B)
+                        {
+                            if (i == 0)
+                            {
+                                map[i, j] = S;
+                            }
+                            else if (map[i - 1, j] != E)
+                            {
+                                map[i, j] = S;
+                                map[i - 1, j] = B;
+                            }
+                            else if (map[i - 1, j] == E)
+                            {
+                                map[i, j] = S;
+                                map[i - 1, j] = S;
+                                enemySzam--;
+                            }
+                        }
+                    }
+                }
+
+                lovesCount = 0;
+            }
+
+            mozgasCount++;
+
+            if (mozgasCount >= 24)
+            {
+                for (int i = map.GetLength(0) - 1; i != 0; i--)
+                {
+                    for (int j = map.GetLength(1) - 1; j >= 0; j--)
+                    {
+
+                        if (map[i - 1, j] == E)
+                        {
+                            if (map[i, j] != B)
+                            {
+                                map[i, j] = map[i - 1, j];
+                                map[i - 1, j] = S;
+                            }
+                            else
+                            {
+                                map[i, j] = S;
+                                map[i - 1, j] = S;
+
+                                enemySzam--;
+                            }
+                        }
+                    }
+                }
+
+                mozgasCount = 0;
+
+                int talal = 0;
+
+                for (int k = map.GetLength(1) - 1; k >= 0; k--)
+                {
+
+                    map[0, k] = ures_sor[k];
+
+                    if (map[8, k] == E && talal == 0)
+                    {
+                        Console.WriteLine("Vesztettél, a Föld elesett!");
+                        kilepes = true;
+                        talal++;
+                    }
+                }
+            }
+
+            if (enemySzam <= 0)
+            {
+                Kiiras();
+
+                Console.WriteLine("Nyertél!");
+
+                Console.WriteLine("Szeretnél megint játszani? Nyomj ENTERT ha igen");
+
+                pressed_key = Console.ReadKey(true);
+
+                if (pressed_key.Key == ConsoleKey.Enter)
+                {
+                    AddEnemy();
+                    Console.Clear();
                 }
                 else
                 {
-                    map[player_y, player_x] = S;
-                    player_x = 0;
-                    map[player_y, player_x] = P;
-                }
-            }
-            
-            if (pressed_key.Key == ConsoleKey.LeftArrow)
-            {
-                if (map[player_y, player_x - 1] != F)
-                {
-                    map[player_y, player_x] = S;
-                    map[player_y, player_x--] = P;
-                }
-                else
-                {
-                    map[player_y, player_x] = S;
-                    player_x = 9;
-                    map[player_y, player_x] = P;
-                }
-            }
-            
-            for (int i = map.GetLength(0) - 1; i != 0; i--)
-            {
-                for (int j = map.GetLength(1) - 1; j >= 0; j--)
-                {
-                    map[i, j] = map[i - 1, j];
-                }
-            }
-            
-            for (int k = map.GetLength(1) - 1; k >= 0; k--)
-            {
-                map[0, k] = ures_sor[k];
-
-                if (map[8, k] == E)
-                {
+                    Console.Clear();
                     kilepes = true;
                 }
             }
-            
-            map[player_y, player_x] = P;
 
 
-            if (pressed_key.Key == ConsoleKey.Escape) kilepes = true;
+            Kiiras();
+
+            Thread.Sleep(1000 / 24);
 
         } while (!kilepes);
 
+
+
         Kiiras();
         Console.WriteLine();
-        Console.WriteLine("Vesztettél!");
+        Console.WriteLine("Viszlát!");
         Console.WriteLine();
         Console.WriteLine("Nyomjon egy gombot a kilépéshez!");
         Console.ReadKey(true);
     }
-    
-    
-    
-    
+
+
+
+
+
     static void Main(string[] args)
     {
-        AddEnemy();
         Mozgas();
     }
 }
